a3579684bb04446d78803030c745acf7795c7512
Hari-sh-code <harishrn2004@gmail.com>
2025-01-07 20:11:52 +0530

    Jenkin

--- cusbotwrk.py
+++ cusbotwrk.py
@@ -3,11 +3,12 @@ from flask_cors import CORS
 import json
 import os
 
+# Initialize Flask app
 app = Flask(__name__, template_folder='template')
-CORS(app)  # Enable CORS for all routes
+CORS(app)
 
-# Load responses from the JSON file
-with open(os.path.join(os.path.dirname(__file__), 'cusbot.json'), 'r') as file:
+# Load predefined responses from JSON
+with open(os.path.join(os.path.dirname(__file__), 'cusbot.json'), 'r', encoding='utf-8') as file:
     responses = json.load(file)
 
 @app.route('/')
@@ -16,9 +17,12 @@ def index():
 
 @app.route('/chat', methods=['POST'])
 def chat():
-    user_message = request.json.get('message', '').lower()
+    user_message = request.json.get('message', '').strip().lower()
+
+    # Check predefined responses from the JSON file
     bot_response = responses.get(user_message, responses.get("default"))
-    return jsonify({"message": bot_response}), 200, {'Content-Type': 'application/json'}
+
+    return jsonify({"message": bot_response}), 200
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
